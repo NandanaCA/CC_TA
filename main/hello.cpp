7d1b0e5ab19dd6a3013b3751941b4529736fc2b2
NandanaCA <nandanacanil@gmail.com>
2025-03-13 15:17:50 +0530

    made changes to hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,7 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "PES2UG22CS336\n";
   cout << "LAB 8\n";
+  cout << "Nandana C A\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
